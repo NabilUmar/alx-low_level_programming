857c5c2a093616312672f0aba3c8c27d2066ad13
NabilUmar <nabilaudu15@gmail.com>
2023-02-19 14:40:48 -0800

    Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description -Write alphabets in lower then upper case
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; 1 < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}	
