7f59c695aa8eb1c26ce6e595ac676471a4560bc9
NabilUmar <nabilaudu15@gmail.com>
2023-02-20 12:36:40 -0800

    Alphabets

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 /**
  * main - Entry point
  *
- * Return: ALways 0 (success)
+ * Return:Always 0 (success)
  */
 
 int main(void)
 {
 	int i;
-
 	for (i = 97; i < 123; i++)
 	{
-		if (i != 101 && i !=113)
+		if (i != 101 && i != 113)
 		{
-			putchar(1);
+		putchar(i);
 		}
 	}
-	putchar('\n');
+putchar('\n');
 return (0);
-}																												
+}
