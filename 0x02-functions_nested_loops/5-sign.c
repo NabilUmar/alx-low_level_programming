a37deefd1b24695984ffa33fe82acdc76a593ea4
NabilUmar <nabilaudu15@gmail.com>
2023-02-21 15:04:12 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,24 +3,24 @@
  * print_sign - function that prints the size of a number
  * @n: int
  * Return:Returns 1 and prints + if n is greater than zero
- * 	Returns 0 and prints 0 if n is zero
- * 	Returns -1 and prints - if n is less than zero
+ * Returns 0 and prints 0 if n is zero
+ * Returns -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-	_putchar('+');
-	return (1);
-	}
-	else if (n == 0)
-	{
-	_putchar('0');
-	return (0);
-	}
-	else 
-	{
-	_putchar('-');
-	return (-1);
-	}
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else 
+{
+_putchar('-');
+return (-1);
+}
 }	
