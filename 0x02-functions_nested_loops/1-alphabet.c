8d5075ac9bbadca313d4f3d2868660b9c126c28f
NabilUmar <nabilaudu15@gmail.com>
2023-02-21 13:40:55 -0800

    Lower case alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return:Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	char c;
+	c = 'a';
+
+	while (c <= 'z')
+	{
+	_putchar(c);
+	c++;
+	}
+_putchar('\n');
+}
