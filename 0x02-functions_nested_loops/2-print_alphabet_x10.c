791ca7e97d19af88f36f78a5a97298e538549888
NabilUmar <nabilaudu15@gmail.com>
2023-02-22 12:32:27 -0800

    Alphabets_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,13 @@ void print_alphabet_x10(void)
 	char chr;
 	int i = 0;
 
-	while (i <= 9)
-	{
+while (i <= 9)
+{
 		for (chr = 'a'; chr <= 'z'; chr++)
 		{
 		_putchar(chr);
 		}
-	_putchar('\n');
-	i++;
-	}
+_putchar('\n');
+i++;
+}
 }	
