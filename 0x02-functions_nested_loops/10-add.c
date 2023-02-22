868f3e4e0ca5f4ac189a628727f04fc3b02979e2
NabilUmar <nabilaudu15@gmail.com>
2023-02-21 16:35:58 -0800

    integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point;
+ * Description:Function that adds two integers and returns the result
+ * Return: Result
+ */
+int add(int a, int b)
+{
+int res;
+
+res = a + b;
+return res;
+}
