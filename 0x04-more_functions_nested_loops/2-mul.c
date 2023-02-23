add3cc7d30807f068d6960c9adad2fc1dadaba6d
NabilUmar <nabilaudu15@gmail.com>
2023-02-23 13:58:48 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - Entry block
+ * Description:Function that multiplies two integers
+ * Return:The result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
