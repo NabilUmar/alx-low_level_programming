5a0b322eea603adbe9e1c1b7b6a7088ba5a23303
NabilUmar <nabilaudu15@gmail.com>
2023-02-27 11:13:45 -0800

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * int _strlen - returns the length of a string
+ *
+ * Return:Always 0
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+	{
+	}
+	return (c);
+}
